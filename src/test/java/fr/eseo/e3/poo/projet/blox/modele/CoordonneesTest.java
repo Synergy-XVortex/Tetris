57aec67eec0879d07e6f7358de1aec4e5dbe7974
Clément Vongsanga <clement.vongsanga@gmail.com>
2025-03-05 09:10:37 +0100

    Update

--- src/test/java/fr/eseo/e3/poo/projet/blox/modele/CoordonneesTest.java
+++ src/test/java/fr/eseo/e3/poo/projet/blox/modele/CoordonneesTest.java
@@ -1,15 +1,15 @@
 package fr.eseo.e3.poo.projet.blox.modele;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class CoordonneesTest {
 
     @Test
     public void testConstructeur() {
         Coordonnees coord = new Coordonnees(5, 10);
-        assertEquals("L'abscisse doit être 5", 5, coord.getAbscisse());
-        assertEquals("L'ordonnée doit être 10", 10, coord.getOrdonnee());
+        assertEquals(5, coord.getAbscisse(), "L'abscisse doit être 5");
+        assertEquals(10, coord.getOrdonnee(), "L'ordonnée doit être 10");
     }
 
     @Test
@@ -17,14 +17,14 @@ public class CoordonneesTest {
         Coordonnees coord = new Coordonnees(0, 0);
         coord.setAbscisse(7);
         coord.setOrdonnee(14);
-        assertEquals("L'abscisse doit être 7", 7, coord.getAbscisse());
-        assertEquals("L'ordonnée doit être 14", 14, coord.getOrdonnee());
+        assertEquals(7, coord.getAbscisse(), "L'abscisse doit être 7");
+        assertEquals(14, coord.getOrdonnee(), "L'ordonnée doit être 14");
     }
 
     @Test
     public void testToString() {
         Coordonnees coord = new Coordonnees(3, 8);
-        assertEquals("La méthode toString() doit retourner (3, 8)", "(3, 8)", coord.toString());
+        assertEquals("(3, 8)", coord.toString(), "La méthode toString() doit retourner (3, 8)");
     }
 
     @Test
@@ -33,14 +33,14 @@ public class CoordonneesTest {
         Coordonnees coord2 = new Coordonnees(2, 6);
         Coordonnees coord3 = new Coordonnees(3, 6);
         
-        assertTrue("Deux objets avec les mêmes coordonnées doivent être égaux", coord1.equals(coord2));
-        assertFalse("Deux objets avec des coordonnées différentes ne doivent pas être égaux", coord1.equals(coord3));
+        assertTrue(coord1.equals(coord2), "Deux objets avec les mêmes coordonnées doivent être égaux");
+        assertFalse(coord1.equals(coord3), "Deux objets avec des coordonnées différentes ne doivent pas être égaux");
     }
 
     @Test
     public void testHashCode() {
         Coordonnees coord1 = new Coordonnees(4, 9);
         Coordonnees coord2 = new Coordonnees(4, 9);
-        assertEquals("Deux objets égaux doivent avoir le même hashCode", coord1.hashCode(), coord2.hashCode());
+        assertEquals(coord1.hashCode(), coord2.hashCode(), "Deux objets égaux doivent avoir le même hashCode");
     }
 }

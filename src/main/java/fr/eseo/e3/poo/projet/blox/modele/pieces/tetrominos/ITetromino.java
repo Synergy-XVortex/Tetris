32f1a0c5f740ddeb536cd678184807041092df64
TTeano <116088023+TTeano@users.noreply.github.com>
2025-03-05 09:43:49 +0100

    Update ITetromino.java

--- src/main/java/fr/eseo/e3/poo/projet/blox/modele/pieces/tetrominos/ITetromino.java
+++ src/main/java/fr/eseo/e3/poo/projet/blox/modele/pieces/tetrominos/ITetromino.java
@@ -1,5 +1,13 @@
 package fr.eseo.e3.poo.projet.blox.modele.pieces.tetrominos;
 
-public class ITetromino {
-
+// Classe ITetromino qui hérite de Tetromino
+public class ITetromino extends Tetromino {
+    public ITetromino(int x, int y) {
+        super(new Element[]{
+            new Element(x, y),
+            new Element(x, y + 1),
+            new Element(x, y + 2),
+            new Element(x, y + 3)
+        });
+    }
 }

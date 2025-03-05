a15c91df55c6448eccf2d1256618b0ec5d868352
TTeano <116088023+TTeano@users.noreply.github.com>
2025-03-05 09:42:42 +0100

    Update Tetromino.java

--- src/main/java/fr/eseo/e3/poo/projet/blox/modele/pieces/tetrominos/Tetromino.java
+++ src/main/java/fr/eseo/e3/poo/projet/blox/modele/pieces/tetrominos/Tetromino.java
@@ -1,5 +1,21 @@
 package fr.eseo.e3.poo.projet.blox.modele.pieces.tetrominos;
 
+// Classe mère Tetromino
 public abstract class Tetromino {
+    protected Element[] elements;
 
+    public Tetromino(Element[] elements) {
+        this.elements = elements;
+    }
+
+    public Element[] getElements() {
+        return elements;
+    }
+
+    public void afficher() {
+        System.out.println(this.getClass().getSimpleName() + " :");
+        for (Element e : elements) {
+            System.out.println("(" + e.getX() + ", " + e.getY() + ")");
+        }
+    }
 }

60aaafc3144be8937ca552c6a545b817810a8344
Clément Vongsanga <clement.vongsanga@gmail.com>
2025-03-26 09:19:50 +0100

    update

--- src/main/java/fr/eseo/e3/poo/projet/blox/modele/pieces/UsineDePiece.java
+++ src/main/java/fr/eseo/e3/poo/projet/blox/modele/pieces/UsineDePiece.java
@@ -1,5 +1,51 @@
 package fr.eseo.e3.poo.projet.blox.modele.pieces;
 
+import java.util.Random;
+
+import fr.eseo.e3.poo.projet.blox.modele.Coordonnees;
+import fr.eseo.e3.poo.projet.blox.modele.Couleur;
+import fr.eseo.e3.poo.projet.blox.modele.pieces.tetrominos.ITetromino;
+import fr.eseo.e3.poo.projet.blox.modele.pieces.tetrominos.OTetromino;
+
 public class UsineDePiece {
 
+    public enum TypePiece {
+        O, I, T, L, J, S, Z
+    }
+
+    private static final Random RANDOM = new Random();
+
+    public static Piece genererPiece() {
+        TypePiece[] types = TypePiece.values();
+        int index = RANDOM.nextInt(types.length);
+    
+        System.out.println("Index généré : " + index + ", Type choisi : " + types[index]); // Log pour debug
+    
+        return genererPiece(types[index]);
+    }
+    
+
+    public static Piece genererPiece(TypePiece type) {
+        Coordonnees position = new Coordonnees(5, 0);
+        Couleur couleur = Couleur.values()[RANDOM.nextInt(Couleur.values().length)];
+
+        switch (type) {
+            case O:
+                return new OTetromino(position, couleur);
+            case I:
+                return new ITetromino(position, couleur);
+            //case T:
+            //    return new TTetromino(position, couleur);
+            //case L:
+            //    return new LTetromino(position, couleur);
+            //case J:
+            //    return new JTetromino(position, couleur);
+            //case S:
+            //    return new STetromino(position, couleur);
+            //case Z:
+            //    return new ZTetromino(position, couleur);
+            default:
+                throw new IllegalArgumentException("Type de pièce inconnu");
+        }
+    }
 }
--- src/test/java/fr/eseo/e3/poo/projet/blox/modele/pieces/UsineDePieceTest.java
+++ src/test/java/fr/eseo/e3/poo/projet/blox/modele/pieces/UsineDePieceTest.java
@@ -1,5 +1,25 @@
 package fr.eseo.e3.poo.projet.blox.modele.pieces;
 
-public class UsineDePieceTest {
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import fr.eseo.e3.poo.projet.blox.modele.pieces.tetrominos.ITetromino;
+import fr.eseo.e3.poo.projet.blox.modele.pieces.tetrominos.OTetromino;
 
+class UsineDePieceTest {
+
+    @RepeatedTest(10)
+    void testGenererPieceAleatoire() {
+        Piece piece = UsineDePiece.genererPiece();
+        assertNotNull(piece, "La pièce générée ne doit pas être null");
+    }
+
+    @Test
+    void testGenererPieceSpecifique() {
+        Piece pieceO = UsineDePiece.genererPiece(UsineDePiece.TypePiece.O);
+        assertTrue(pieceO instanceof OTetromino, "La pièce générée doit être un OTetromino");
+
+        Piece pieceI = UsineDePiece.genererPiece(UsineDePiece.TypePiece.I);
+        assertTrue(pieceI instanceof ITetromino, "La pièce générée doit être un ITetromino");
+    }
 }

0b3862d10528356f4f854573b1c59acb7fb8466e
Clément Vongsanga <clement.vongsanga@gmail.com>
2025-04-02 09:24:43 +0200

    Update

--- src/main/java/fr/eseo/e3/poo/projet/blox/modele/Couleur.java
+++ src/main/java/fr/eseo/e3/poo/projet/blox/modele/Couleur.java
@@ -1,11 +1,23 @@
 package fr.eseo.e3.poo.projet.blox.modele;
 
+import java.awt.Color;
+
 public enum Couleur {
-    ROUGE,
-    ORANGE,
-    BLEU,
-    VERT,
-    JAUNE,
-    CYAN,
-    VIOLET;
+    BLEU(Color.BLUE),
+    ROUGE(Color.RED),
+    JAUNE(Color.YELLOW),
+    VERT(Color.GREEN),
+    ORANGE(Color.ORANGE),
+    CYAN(Color.CYAN),
+    VIOLET(new Color(128, 0, 128)); // Violet n'existe pas en Color par défaut
+
+    private final Color awtColor;
+
+    Couleur(Color awtColor) {
+        this.awtColor = awtColor;
+    }
+
+    public Color getAwtColor() {
+        return awtColor;
+    }
 }

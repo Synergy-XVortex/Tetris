36a82ad95edc7b0ba3acdc94a0db6c2a30aaf8bc
Clément Vongsanga <clement.vongsanga@gmail.com>
2025-04-04 11:32:17 +0200

    Update

--- src/test/java/fr/eseo/e3/poo/projet/blox/modele/CouleurTest.java
+++ src/test/java/fr/eseo/e3/poo/projet/blox/modele/CouleurTest.java
@@ -9,13 +9,13 @@ public class CouleurTest {
     @Test
     public void testCouleursCorrespondent() {
         // Vérifie que chaque couleur de l'énumération a une valeur correcte
-        assertEquals(new Color(30, 144, 255), Couleur.BLEU.getAwtColor(), "BLEU incorrect");
-        assertEquals(new Color(220, 20, 60), Couleur.ROUGE.getAwtColor(), "ROUGE incorrect");
-        assertEquals(new Color(255, 223, 0), Couleur.JAUNE.getAwtColor(), "JAUNE incorrect");
-        assertEquals(new Color(50, 205, 50), Couleur.VERT.getAwtColor(), "VERT incorrect");
-        assertEquals(new Color(255, 140, 0), Couleur.ORANGE.getAwtColor(), "ORANGE incorrect");
+        assertEquals(new Color(0, 0, 255), Couleur.BLEU.getAwtColor(), "BLEU incorrect");
+        assertEquals(new Color(255, 0, 0), Couleur.ROUGE.getAwtColor(), "ROUGE incorrect");
+        assertEquals(new Color(255, 255, 0), Couleur.JAUNE.getAwtColor(), "JAUNE incorrect");
+        assertEquals(new Color(0, 255, 0), Couleur.VERT.getAwtColor(), "VERT incorrect");
+        assertEquals(new Color(255, 200, 0), Couleur.ORANGE.getAwtColor(), "ORANGE incorrect");
         assertEquals(new Color(0, 255, 255), Couleur.CYAN.getAwtColor(), "CYAN incorrect");
-        assertEquals(new Color(138, 43, 226), Couleur.VIOLET.getAwtColor(), "VIOLET incorrect");
+        assertEquals(new Color(128, 0, 128), Couleur.VIOLET.getAwtColor(), "VIOLET incorrect");
     }
 
     @Test
